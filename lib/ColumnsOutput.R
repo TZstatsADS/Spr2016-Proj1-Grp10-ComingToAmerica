9036d33870639c62185fda8d165964a3dd660ff2
Zhibo Wan <zw2327@columbia.edu>
2016-01-29 23:41:32 -0500

    use fread

--- lib/ColumnsOutput.R
+++ lib/ColumnsOutput.R
@@ -8,7 +8,7 @@ getwd()
 setwd("D:\\ADSProject\\project1\\cycle1-10\\lib")
 
 #using read.csv(), this could take a while 
-ss13pusa=read.csv(file="../data/ss13pusa.csv",header=TRUE,sep=",")
+ss13pusa=fread(file="../data/ss13pusa.csv",header=TRUE)
 
 #for this line you should input the columns you want 
 data=data.frame(ss13pusa$WAGP,ss13pusa$ANC1P,ss13pusa$ANC2P)

b7128509b96fc9932ce754346a85456ebee994c0
Zhibo Wan <zw2327@columbia.edu>
2016-01-29 23:12:10 -0500

    added

---
+++ lib/ColumnsOutput.R
@@ -0,0 +1,18 @@
+library(dplyr)
+library(readr)
+
+#get your current working directory
+getwd()
+
+#Please set it to your own work directory, for window users, you must use"//" instead of "/"
+setwd("D:\\ADSProject\\project1\\cycle1-10\\lib")
+
+#using read.csv(), this could take a while 
+ss13pusa=read.csv(file="../data/ss13pusa.csv",header=TRUE,sep=",")
+
+#for this line you should input the columns you want 
+data=data.frame(ss13pusa$WAGP,ss13pusa$ANC1P,ss13pusa$ANC2P)
+
+# this lines output data to csv file 
+write.csv(data,file="../data/output.csv",row.names=FALSE)
+
